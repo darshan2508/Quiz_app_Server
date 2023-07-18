fd561c48a8c256becb87034ff7a89d385bbc13d1
darshanChandan <darshanchandan2508@gmail.com>
2023-07-18 19:51:22 +0530

    connection string direct

--- database/conn.js
+++ database/conn.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
 export default async function connect() {
-  await mongoose.connect(process.env.ATLAS_URI);
+  await mongoose.connect(
+    "mongodb+srv://Admin:Admin123@quiz.udgevb4.mongodb.net/?retryWrites=true&w=majority"
+  );
   console.log("Database connected");
 }
